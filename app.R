32da30250c1a23c10d9e795cffda743e22db1edc
Atsuhiro Kou <millerttime60@gmail.com>
2018-01-19 00:10:09 +0900

    1. repair bags bags that SOC of CTCAEv5 was not displayed are repaired
    2. add new function
    add search for term function
    
    3. add reference and dictionary link
    add reference of CTCAE and dictionary link

--- app.R
+++ app.R
@@ -48,13 +48,25 @@ ui <- fluidPage(
                                                      "Vascular" = "Vascular disorders"),
                                       selected = "All"
                                         ), 
+                   textInput("term", label = h3("Search Term"), value = ""), 
                    sliderInput("Grade", label = h3("CTCAE Grade"), 
-                               min = 1, max = 5, value = c(1, 5))
+                               min = 1, max = 5, value = c(1, 5)), 
+                   h3("Reference"), 
+                   tags$a(href = "https://ctep.cancer.gov/protocoldevelopment/electronic_applications/ctc.htm", 
+                          "National Institutes of health (NIH)"), 
+                   br(), 
+                   tags$a(href = "http://www.jcog.jp/doctor/tool/CTCAEv4J_20100911.pdf", 
+                          "JCOG (Japanese, v4.03)"), 
+                   h3("Search English"), 
+                   tags$a(href = "https://lsd-project.jp/cgi-bin/lsdproj/ejlookup04.pl", 
+                          "Life Science Dictionary"), 
+                   br(), 
+                   tags$a(href = "https://ejje.weblio.jp", "Weblio Dictionary")
                    ), 
     mainPanel(width = 9, tabsetPanel(type = "tabs",
                          tabPanel("ver.4.0.3", DT::dataTableOutput("dtl1")),
                          tabPanel("ver 5.0", DT::dataTableOutput("dtl2")))
-             ) 
+              ) 
   )
 )
 
@@ -68,10 +80,17 @@ server <- function(input, output) {
   })
   
   CTCAEv5_subset1 <- reactive({
-    display_column(CTCAEv5, input$display, "MedDRA") %>% 
+    if(str_detect_all(input$display, "MedDRA")) {
+      CTCAEv5 %>%
       display_column(input$display, "Definition") %>% 
       display_column(input$display, "Note") %>% 
       display_column(input$display, "Change")
+    }else{
+      CTCAEv5 %>%  select(- MedDRA_Code) %>% 
+      display_column(input$display, "Definition") %>% 
+      display_column(input$display, "Note") %>% 
+      display_column(input$display, "Change")
+    }
   })
   
   CTCAEv4_subset2 <- reactive({
@@ -86,21 +105,37 @@ server <- function(input, output) {
     if(input$SOC == "All") {
       return(CTCAEv5_subset1())
     }else{
-      filter(CTCAEv5_subset1(), CTCAE_v4.0_SOC == input$SOC)
+      filter(CTCAEv5_subset1(), MedDRA_SOC == input$SOC)
     }
   })
   
   CTCAEv4_subset3 <- reactive({
-    select_grade(CTCAEv4_subset2(), input$Grade[1], input$Grade[2])
+    if(input$term == "") {
+      return(CTCAEv4_subset2())
+    }else{
+      filter(CTCAEv4_subset2(), str_detect(str_to_lower(CTCAE_v4.0_Term), input$term))
+    }
   })
   
   CTCAEv5_subset3 <- reactive({
-    select_grade(CTCAEv5_subset2(), input$Grade[1], input$Grade[2])
+    if(input$term == "") {
+      return(CTCAEv5_subset2())
+    }else{
+      filter(CTCAEv5_subset2(), str_detect(str_to_lower(CTCAE_Term), input$term))
+    }
   })
   
-  output$dtl1 <- DT::renderDataTable(CTCAEv4_subset3())
+  CTCAEv4_subset4 <- reactive({
+    select_grade(CTCAEv4_subset3(), input$Grade[1], input$Grade[2])
+  })
   
-  output$dtl2 <- DT::renderDataTable(CTCAEv5_subset3())
+  CTCAEv5_subset4 <- reactive({
+    select_grade(CTCAEv5_subset3(), input$Grade[1], input$Grade[2])
+  })
+  
+  output$dtl1 <- DT::renderDataTable(CTCAEv4_subset4())
+  
+  output$dtl2 <- DT::renderDataTable(CTCAEv5_subset4())
 }
 
 # Run the app
